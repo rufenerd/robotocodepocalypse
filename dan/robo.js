89ad518dadadbc6493f95a20c69ca4f745af51f3
Dan Rufener <dan@deckmonitoring.com>
2013-03-15 00:24:11 -0700

    walls

--- dan/robo.js
+++ dan/robo.js
@@ -1,15 +1,37 @@
 window.onload = function(){
-  Crafty.init(900, 600);
+  CANVAS_HEIGHT = 600;
+  CANVAS_WIDTH = 900;
+
+  Crafty.init(CANVAS_WIDTH, CANVAS_HEIGHT);
   Crafty.background('rgb(127,127,127)');
   TILE_SIZE = 5;
   TURN_TIME = 1;
 
+  //Walls
+  var leftWall = Crafty.e("2D, solid")
+    .attr({ x: -1, y: 0, w: 1, h: CANVAS_HEIGHT});
+
+  var rightWall = Crafty.e("2D, solid")
+    .attr({ x: CANVAS_WIDTH, y: 0, w: 1, h: CANVAS_HEIGHT});
+
+  var topWall = Crafty.e("2D, solid")
+    .attr({ x: 0, y: -1, w: CANVAS_WIDTH, h: 1});
+
+  var leftWall = Crafty.e("2D, solid")
+    .attr({ x: 0, y: CANVAS_HEIGHT, w: CANVAS_WIDTH, h: 1});
+
+
   //Unit
-  var unit = Crafty.e("2D, Canvas, Color, Multiway, Delay")
+  var unit = Crafty.e("2D, Canvas, Color, Multiway, Delay, Collision")
 	  .attr({ x: 300, y: 150, w: 10, h: 10})
 	  .color('rgb(0,0,255)')
     .multiway(1, {})
-    .disableControl();
+    .disableControl()
+    .bind('Moved', function(from) {
+      if(this.hit('solid')){
+        this.attr({x: from.x, y:from.y});
+      }
+    });
 
   unit.execute = function(cmd){
     cmd = cmd.split(" ");
@@ -20,6 +42,9 @@ window.onload = function(){
       if (distanceToTravel === 0) {
         return;
       } else {
+        var oldX = this.x;
+        var oldY = this.y;
+
         switch (cmd[1]) {
         case "north":
         case "n":
@@ -40,10 +65,10 @@ window.onload = function(){
         }
 
         this.attr({ x: this.x, y: this.y });
+        this.trigger("Moved", { x: oldX, y: oldY });
         distanceToTravel -= 1;
 
         if (distanceToTravel) {
-          console.log(this);
           this.delay(function(){
             this.execute([cmd[0], cmd[1], distanceToTravel].join(" "));
           }, TURN_TIME)
