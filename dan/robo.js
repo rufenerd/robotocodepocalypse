eee6980d0f23f8f3d6d16431d14a5b2396065a75
Dan Rufener <dan@deckmonitoring.com>
2013-03-14 23:36:57 -0700

    slow down movement with a timeout, set default on input and misc

--- dan/robo.js
+++ dan/robo.js
@@ -1,6 +1,7 @@
 window.onload = function(){
   Crafty.init(900, 600);
   Crafty.background('rgb(127,127,127)');
+  TILE_SIZE = 10;
 
   //Unit
   var unit = Crafty.e("2D, Canvas, Color, Multiway")
@@ -13,32 +14,38 @@ window.onload = function(){
     cmd = cmd.split(" ");
 
     if (cmd[0] == "move" || cmd[0] == "m") {
-      var numLeftToMove = _.isUndefined(cmd[2]) ? 1 : parseInt(cmd[2]);
+      var distanceToTravel = _.isUndefined(cmd[2]) ? 1 : parseInt(cmd[2]);
 
-      if (numLeftToMove === 0) {
+      if (distanceToTravel === 0) {
         return;
       } else {
         switch (cmd[1]) {
         case "north":
         case "n":
-          this.y -= 10;
+          this.y -= TILE_SIZE;
           break;
         case "south":
         case "s":
-          this.y += 10;
+          this.y += TILE_SIZE;
           break;
         case "east":
         case "e":
-          this.x += 10;
+          this.x += TILE_SIZE;
           break;
         case "west":
         case "w":
-          this.x -= 10;
+          this.x -= TILE_SIZE;
           break;
         }
 
         this.attr({ x: this.x, y: this.y });
-        unit.execute([cmd[0], cmd[1], numLeftToMove - 1].join(" "));
+        distanceToTravel -= 1;
+
+        if (distanceToTravel) {
+          this.timeout(function(){
+            unit.execute([cmd[0], cmd[1], distanceToTravel].join(" "));
+          }, 30)
+        }
       }
     }
   }
