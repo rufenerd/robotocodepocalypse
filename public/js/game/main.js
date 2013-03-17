bf24da5b2c2a5da52ae2660aa2c3afcc07f2c09d
Dan Rufener <dan@deckmonitoring.com>
2013-03-16 20:37:28 -0700

    working with joe, moved io stuff into separated files that used to be robo.js

--- public/js/game/main.js
+++ public/js/game/main.js
@@ -9,8 +9,21 @@ define(function(require) {
     TURN_TIME    : 1,
 
     initialize: function() {
+      this.server = io.connect('http://localhost:3000');
+      var server = this.server;
+      
+      server.on('connect', function(data) {
+        nickname = prompt("What is your nickname?");
+        server.emit('join', nickname);
+      });
+
+      server.on('newState', function (newState) {
+        console.log(newState);
+      });
+
       Crafty.init(this.CANVAS_WIDTH, this.CANVAS_HEIGHT);
       Crafty.background('rgb(127,127,127)');
+
 
       // Walls
       Crafty.e("2D, solid").attr({ x: 0, y: -1, w: this.CANVAS_WIDTH, h: 1 });
