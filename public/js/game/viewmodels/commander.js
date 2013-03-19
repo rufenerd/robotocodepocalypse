a21f6610a8b62e0b606fc5e6bc5bfa54144e3cb6
Joe Gores <jgores@webmd.net>
2013-03-19 13:46:29 -0700

    add history functionality to commander

--- public/js/game/viewmodels/commander.js
+++ public/js/game/viewmodels/commander.js
@@ -4,21 +4,61 @@ define(function(require){
 
   var self = ViewModel.extend({
     el: document.getElementById('commander'),
-    command: ko.observable('m s 5'),
+    history: [],
+    historyIndex: 0,
+    command: ko.observable(''),
     inputHasFocus: ko.observable(true),
 
-    onKeyDown: function(view, keyboardEvent){
-      if (keyboardEvent.keyCode === 13) {
-        keyboardEvent.preventDefault();
-        self.inputHasFocus(false);
-        self.execute();
-        return false;
+    previousHistory: function() {
+      var result = false;
+      if (self.history.length) {
+        self.historyIndex--;
+        self.historyIndex = self.historyIndex < 0 ? 0 : self.historyIndex;
+        result = self.history[self.historyIndex];
+      }
+      return result;
+    },
+
+    nextHistory: function() {
+      var result = false;
+      if (self.history.length) {
+        self.historyIndex++;
+        self.historyIndex = self.historyIndex > self.history.length - 1  ? self.history.length - 1 : self.historyIndex;
+        result = self.history[self.historyIndex];
+      }
+      return result;
+    },
+
+    onKeyDown: function(view, keyboardEvent) {
+      switch (keyboardEvent.keyCode) {
+        case 13: // enter key
+          keyboardEvent.preventDefault();
+          self.inputHasFocus(false);
+          self.execute();
+          return false;
+
+        case 38: // up arrow
+          keyboardEvent.preventDefault();
+          var cmd = self.previousHistory();
+          if( cmd ) { self.command(cmd); }
+          return false;
+
+        case 40: // down arrow
+          keyboardEvent.preventDefault();
+          var cmd = self.nextHistory();
+          if( cmd ) { self.command(cmd); }
+          return false;
       }
       return true;
     },
 
-    execute: function(){
-      self.trigger('executecommand', self.command());
+    execute: function() {
+      var cmd = self.command();
+      if(cmd && (!self.history.length || self.history.length && cmd != self.history[self.history.length - 1])){
+        self.history.push(cmd);
+        self.trigger('executecommand', cmd);
+      }
+      self.historyIndex = self.history.length;
       self.command('');
       self.inputHasFocus(true);
     }
